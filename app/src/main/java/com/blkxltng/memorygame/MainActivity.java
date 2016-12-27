2deed6501cd690bae6831b7a478fdb2d29224962
blkxltng <firejama@gmail.com>
2016-12-26 19:49:09 -0500

    Initial commit

---
+++ app/src/main/java/com/blkxltng/memorygame/MainActivity.java
@@ -0,0 +1,27 @@
+package com.blkxltng.memorygame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.button) {
+            Intent i = new Intent(this, GameActivity.class);
+            startActivity(i);
+        }
+    }
+}
